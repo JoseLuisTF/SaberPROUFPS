6d59f4d472c125128a0f9273f9585ca08488b909
Jose Tarazona <joseluistf@ufps.edu.co>
2021-06-13 23:38:07 -0500

    heroku settings changed

--- saberproufps/settings/server.py
+++ saberproufps/settings/server.py
@@ -1,22 +1,2 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['saberproufps.herokuapp.com']
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "d4m72co4s51vvj",
-        "USER": "ioxmuxwysckuin",
-        "PASSWORD": "78e36a7cba8d0ebf593b3487cfff0612271b321aff3091de292192acc5b23955",
-        "HOST": "ec2-23-23-128-222.compute-1.amazonaws.com",
-        "PORT": "5432",
-    }
-}
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '../static'),)
--- saberproufps/wsgi.py
+++ saberproufps/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saberproufps.settings.server')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saberproufps.settings')
 
 application = Cling(get_wsgi_application())
